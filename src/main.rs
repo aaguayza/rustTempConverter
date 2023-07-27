05ac134392b5a9dc7cee426db63e7440ec5d3db9
Tony <adaguayza@gmail.com>
2023-07-27 16:26:20 -0500

    f-c converter done

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@
 //from f - c -> -35 then multiply by 1.8
 
 
-fn convert_to_fahrenheit(x: i32) -> i32{
-    let new_temp = (x * (5/9)) + 35;
-    new_temp 
+fn convert_to_fahrenheit(x: f32) -> f32{
+    
+    let new_temp:f32 = (x * 1.8) + 35.0;
+    new_temp
 }
 fn main() {
-    let temp: i32 = convert_to_fahrenheit(32);
+    let temp: f32 = convert_to_fahrenheit(32.0);
     println!("The new temperature is: {}", temp); 
 }
