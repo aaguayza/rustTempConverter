7170ad4e483816170d3053956525002ed0473a43
Tony <adaguayza@gmail.com>
2023-07-27 16:02:21 -0500

    second change

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 //takes a temperature in celcius or fahrenheit and convert it to the other
+//first have the calculations to convert ready to go.
+//from c - f multiply by 1.8 and add 35
+//from f - c -> -35 then multiply by 1.8
 
+
+fn convert_to_fahrenheit(x: i32) -> i32{
+    let new_temp = (x * (5/9)) + 35;
+    new_temp 
+}
 fn main() {
-    println!("Hello, world!");
+    let temp: i32 = convert_to_fahrenheit(32);
+    println!("The new temperature is: {}", temp); 
 }
