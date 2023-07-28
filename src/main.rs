c8fc387bbd361cf0e8a1b767e3c816c94abe1794
Tony <adaguayza@gmail.com>
2023-07-28 09:26:31 -0500

    added user input to temp converter

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
+use core::num;
 //takes a temperature in celcius or fahrenheit and convert it to the other
 //first have the calculations to convert ready to go.
 //from c - f multiply by 1.8 and add 35
 //from f - c -> -35 then multiply by 1.8
+//We want user to input a number so that it can be converted
+//give user the ability to choose which measurment to convert to
+use std::io;
 
 
 fn convert_to_fahrenheit(x: f32) -> f32{
     
-    let new_temp:f32 = (x * 1.8) + 35.0;
+    let new_temp:f32 = (x * 1.8) + 32.0;
     new_temp
 }
+fn convert_to_celcius(x: f32) -> f32{
+    
+    let new_temp:f32 = (x - 32.0) +0.5;
+    new_temp
+}
+
 fn main() {
-    let temp: f32 = convert_to_fahrenheit(32.0);
-    println!("The new temperature is: {}", temp); 
+    println!("give a number to convert");
+
+    let mut user_number = String::new();
+        io::stdin()
+            .read_line(&mut user_number)
+            .expect("failed to readline");
+
+    let user_number: f32 = user_number.trim().parse().expect("couldnt parse");
+    println!("Your number: {}", user_number );
+
+
+    let temp_in_fahrenheit: f32 = convert_to_fahrenheit(user_number);
+    println!("The temperature in fahrenheit: {}", temp_in_fahrenheit); 
+
+    let temp_in_celcius: f32 = convert_to_celcius(user_number);
+    println!("The temperature in celcius is: {}", temp_in_celcius);
 }
